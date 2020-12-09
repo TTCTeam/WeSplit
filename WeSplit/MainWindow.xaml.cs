9ab88fde8f3c64a8b7f111cf8a80ca8b6f0d3d33
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-12-09 22:47:51 +0700

    Add HomeScreen

--- WeSplit/MainWindow.xaml.cs
+++ WeSplit/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,69 @@ namespace WeSplit
         {
             InitializeComponent();
         }
+
+        class Place // DTO = Data transfer object - Entity
+        {
+            public string Name { get; set; }
+            public string Thumbnail { get; set; }
+        }
+
+        // DAO - Data access object
+
+        class PlaceDao
+        {
+            public static BindingList<Place> GetAll()
+            {
+                var result = new BindingList<Place>()
+                {
+                    new Place() { Name="Đồng bằng sông Cửu Long", Thumbnail="/Images/PlaceImages/DongBangSongCuuLong.jpg" },
+                    new Place() { Name="Hồ Gươm", Thumbnail="/Images/PlaceImages/HoGuom.jpg" },
+                    new Place() { Name="Nhà hát lớn Hà Nội", Thumbnail="/Images/PlaceImages/NhaHatLonHaNoi.jpg" },
+                    new Place() { Name="Phố cổ Hội An", Thumbnail="/Images/PlaceImages/PhoCoHoiAn.jpg" },
+                    new Place() { Name="Phú Quốc", Thumbnail="/Images/PlaceImages/PhuQuoc.jpg" },
+                    new Place() { Name="Quảng trường Ba Đình", Thumbnail="/Images/PlaceImages/QuangTruongBaDinh.jpg" },
+                    new Place() { Name="Ruộng Bậc Thang", Thumbnail="/Images/PlaceImages/RuongBacThang.jpg" },
+                    new Place() { Name="Vịnh Hạ Long", Thumbnail="/Images/PlaceImages/VinhHaLong.jpg" },
+                };
+
+                return result;
+            }
+        }
+
+        BindingList<Place> _list = new BindingList<Place>();
+
+        Place selected;
+        List<Place> list;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _list = PlaceDao.GetAll();
+            dataListView.ItemsSource = _list;
+        }
+
+        private void dataListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selected = dataListView.SelectedItem as Place;
+
+            MessageBox.Show(selected.Name);
+        }
+
+        private void DetailButton_Click(object sender, RoutedEventArgs e)
+        {
+            selected = (sender as Button).DataContext as Place;
+
+            MessageBox.Show(selected.Name);
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FlagButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
